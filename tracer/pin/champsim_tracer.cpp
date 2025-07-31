d710f8f1fdb76618d447496c9ebf9c7c90f0b75b
Vishnu Ramadas <vramadas@outlook.com>
2025-07-31 14:15:48 -0500

    Update pintool tracer to trace ROIs

--- tracer/pin/champsim_tracer.cpp
+++ tracer/pin/champsim_tracer.cpp
@@ -21,6 +21,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 #include <stdlib.h>
 #include <string.h>
 #include <string>
@@ -40,6 +41,13 @@ std::ofstream outfile;
 
 trace_instr_format_t curr_instr;
 
+const CHAR * ROI_BEGIN = "__parsec_roi_begin";
+const CHAR * ROI_END = "__parsec_roi_end";
+
+FILE * trace;
+ADDRINT ROIStartAddress = 0x0;
+ADDRINT ROIEndAddress = 0xffffffffffffffff;
+
 /* ===================================================================== */
 // Command line switches
 /* ===================================================================== */
@@ -48,6 +56,12 @@ KNOB<std::string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "champsim.
 KNOB<UINT64> KnobSkipInstructions(KNOB_MODE_WRITEONCE, "pintool", "s", "0", "How many instructions to skip before tracing begins");
 
 KNOB<UINT64> KnobTraceInstructions(KNOB_MODE_WRITEONCE, "pintool", "t", "1000000", "How many instructions to trace");
+
+KNOB<UINT64> KnobROIStartAddress(KNOB_MODE_WRITEONCE, "pintool", "roiStart", "0", "Offset of the ROI function within the text segment");
+
+KNOB<UINT64> KnobROIEndAddress(KNOB_MODE_WRITEONCE, "pintool", "roiEnd", "0x1000", "Offset of the ROI function within the text segment");
+
+KNOB<std::string> KnobBMKName(KNOB_MODE_WRITEONCE, "pintool", "bmkName", "stream", "Name of the ROI to be profiled.");
 
 /* ===================================================================== */
 // Utilities
@@ -79,10 +93,21 @@ void ResetCurrentInstruction(VOID* ip)
   curr_instr.ip = (unsigned long long int)ip;
 }
 
-BOOL ShouldWrite()
+BOOL ShouldWrite(void* ip)
 {
-  ++instrCount;
-  return (instrCount > KnobSkipInstructions.Value()) && (instrCount <= (KnobTraceInstructions.Value() + KnobSkipInstructions.Value()));
+  bool shouldWrite = false;
+  ADDRINT addr = (ADDRINT)ip;
+  if (addr >= ROIStartAddress && addr < ROIEndAddress) {
+    ++instrCount;
+    //std::cerr << "Addr " << std::hex << addr << " ROIStartAddress " << ROIStartAddress << " ROIEndAddress " << ROIEndAddress << std::dec << std::endl;
+    shouldWrite = ((instrCount > KnobSkipInstructions.Value()) && (instrCount <= (KnobSkipInstructions.Value() + KnobTraceInstructions.Value())));
+  }
+
+  if (shouldWrite && (instrCount % 100000000) == 0) {
+    std::cerr << " Another 100 million instructions gone by. instrCount = " << instrCount << std::endl << std::flush;
+  }  
+
+  return shouldWrite;
 }
 
 void WriteCurrentInstruction()
@@ -106,6 +131,13 @@ void WriteToSet(T* begin, T* end, UINT32 r)
   *found_reg = r;
 }
 
+template <typename T>
+void PrintInstAddr(T begin, void* ip, UINT32 r) {
+  ADDRINT addr = (ADDRINT)ip;
+  if (addr >= ROIStartAddress && addr < ROIEndAddress)
+  std::cout << "At inst: 0x" << std::hex << addr << " Source Addr: 0x" <<  (ADDRINT)begin << " Operand: 0x" << r << std::dec << std::endl;
+}
+
 /* ===================================================================== */
 // Instrumentation callbacks
 /* ===================================================================== */
@@ -113,7 +145,7 @@ void WriteToSet(T* begin, T* end, UINT32 r)
 // Is called for every instruction and instruments reads and writes
 VOID Instruction(INS ins, VOID* v)
 {
-  // begin each instruction with this function
+    // begin each instruction with this function
   INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)ResetCurrentInstruction, IARG_INST_PTR, IARG_END);
 
   // instrument branch instructions
@@ -138,20 +170,54 @@ VOID Instruction(INS ins, VOID* v)
 
   // instrument memory reads and writes
   UINT32 memOperands = INS_MemoryOperandCount(ins);
-
+  //const xed_decoded_inst_t xedd = *INS_XedDec(ins);
+  //if (xed_decoded_inst_get_attribute(&xedd, XED_ATTRIBUTE_SIMD_SCALAR)) {
+  //  if (INS_Address(ins) >= ROIStartAddress && INS_Address(ins) < ROIEndAddress)
+  //  std::cerr << "Scalar instruction encountered at ins " << std::hex << INS_Address(ins) << ". ROI Start is " << ROIStartAddress <<std::dec << std::endl;
+  //}
   // Iterate over each memory operand of the instruction.
   for (UINT32 memOp = 0; memOp < memOperands; memOp++) {
-    if (INS_MemoryOperandIsRead(ins, memOp))
+    if (INS_MemoryOperandIsRead(ins, memOp)) {
       INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)WriteToSet<unsigned long long int>, IARG_PTR, curr_instr.source_memory, IARG_PTR,
                      curr_instr.source_memory + NUM_INSTR_SOURCES, IARG_MEMORYOP_EA, memOp, IARG_END);
+//      INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)PrintInstAddr<unsigned long long int>, IARG_PTR, curr_instr.source_memory, IARG_INST_PTR,
+//                    IARG_MEMORYOP_EA, memOp, IARG_END);
+    }
     if (INS_MemoryOperandIsWritten(ins, memOp))
       INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)WriteToSet<unsigned long long int>, IARG_PTR, curr_instr.destination_memory, IARG_PTR,
                      curr_instr.destination_memory + NUM_INSTR_DESTINATIONS, IARG_MEMORYOP_EA, memOp, IARG_END);
   }
 
   // finalize each instruction with this function
-  INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR)ShouldWrite, IARG_END);
-  INS_InsertThenCall(ins, IPOINT_BEFORE, (AFUNPTR)WriteCurrentInstruction, IARG_END);
+  ADDRINT addr = INS_Address(ins);
+  if (addr >= ROIStartAddress && addr < ROIEndAddress) {
+//      std::cerr << "addr : 0x" << std::hex << addr << " ROI start: 0x" << ROIStartAddress << " ROI end: 0x" << ROIEndAddress << std::dec << std::endl << std::flush;
+      INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR)ShouldWrite, IARG_INST_PTR, IARG_END);
+      INS_InsertThenCall(ins, IPOINT_BEFORE, (AFUNPTR)WriteCurrentInstruction, IARG_END);
+  }
+
+}
+
+VOID ImageLoad(IMG img, VOID *v)
+{
+    if (IMG_Name(img).find(KnobBMKName) != std::string::npos) {
+        std::cerr << "Image name " << IMG_Name(img) << std::endl;
+        for (SEC sec = IMG_SecHead(img); SEC_Valid(sec); sec = SEC_Next(sec))
+        {
+            for (RTN rtn = SEC_RtnHead(sec); RTN_Valid(rtn); rtn = RTN_Next(rtn))
+            {
+                if (RTN_Name(rtn) == "_init") {
+                    RTN_Open(rtn);
+                    std::cout << "Found routine: " << RTN_Name(rtn) << " at 0x" <<std::hex << RTN_Address(rtn) <<std::dec<< " in section " << SEC_Name(sec) << std::endl;
+                    ROIStartAddress = RTN_Address(rtn) + KnobROIStartAddress.Value();
+                    ROIEndAddress = RTN_Address(rtn) + KnobROIEndAddress.Value();
+                    std::cerr << std::hex << " ROI start: 0x" << ROIStartAddress << " ROI end: 0x" << ROIEndAddress << std::dec << std::endl;
+
+                    RTN_Close(rtn);
+                }
+            }
+        }
+    }
 }
 
 /*!
@@ -182,6 +248,9 @@ int main(int argc, char* argv[])
     std::cout << "Couldn't open output trace file. Exiting." << std::endl;
     exit(1);
   }
+
+  IMG_AddInstrumentFunction(ImageLoad,0);
+  // Register function to be called to instrucment ROI
 
   // Register function to be called to instrument instructions
   INS_AddInstrumentFunction(Instruction, 0);
